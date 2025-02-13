ce61d4196b350d9b0be825dae70191e460123d68
babukre1 <108317783+babukre1@users.noreply.github.com>
2025-02-13 20:18:50 +0300

    commiting rest of the files

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,42 +1,46 @@
 package api
 
-import (
-	"database/sql"
-	"ecommerce-api/service/user"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
-)
-
 // import (
-// 	// "ecommerce-api/service/user"
+// 	"database/sql"
+// 	"ecommerce-api/service/user"
+// 	"encoding/json"
 // 	"log"
-// 	// "log"
+// 	"net/http"
+
+// 	"github.com/gorilla/mux"
+// 	"golang.org/x/text/number"
 // )
 
-// // "golang-api-ecommerce/cmd/api"
+// // import (
+// // 	// "ecommerce-api/service/user"
+// // 	"log"
+// // 	// "log"
+// // )
 
+// // // "golang-api-ecommerce/cmd/api"
 
-type APIServer struct {
+// type APIServer struct {
+// 	addr string
+// 	db   *sql.DB
+// }
+
+// func NewApiServer(addr string, db *sql.DB) *APIServer {
+// 	return &APIServer{
+// 		addr: addr,
+// 		db:   db,
+// 	}
+// }
+
+// func (s *APIServer) Run() error {
+// 	router := mux.NewRouter()
+// 	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+// 	userHandler := user.NewHandler()
+// 	userHandler.RegisterRoutes(subrouter)
+// 	log.Printf("Starting on port %v ", s.addr)
+// 	return http.ListenAndServe(s.addr, router)
+// }
+
+type api struct {
 	addr string
-	db   *sql.DB
-}
-
-
-func NewApiServer(addr string, db *sql.DB)*APIServer{
-	return &APIServer{
-		addr: addr,
-		db: db,
-	}
-}
-
-func (s *APIServer) Run() error{
-	router := mux.NewRouter()
-	subrouter := router.PathPrefix("/api/v1").Subrouter()
-
-	userHandler := user.NewHandler()
-	userHandler.RegisterRoutes(subrouter)
-	log.Printf("Starting on port %v ", s.addr)
-	return http.ListenAndServe(s.addr, router)
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+
 )
+
+
 
 // "ecommerce-api/cmd/api"
 // "log"
@@ -20,21 +24,22 @@ type api struct {
 }
 
 func (s *api) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// w.Write([]byte("Hello From the Server"))
-	switch r.Method {
-	case http.MethodGet:
-		switch r.URL.Path {
-		case "/":
-			w.Write([]byte("Index Page"))
-			return
-		case r.URL.Path:
-			w.Write([]byte(r.URL.Path))
-			return
-		}
-	default:
-		w.Write([]byte("404 not found"))
-		return
-	}
+	
+	w.Write([]byte("Hello From the Server"))
+	// switch r.Method {
+	// case http.MethodGet:
+	// 	switch r.URL.Path {
+	// 	case "/":
+	// 		w.Write([]byte("Index Page"))
+	// 		return
+	// 	case r.URL.Path:
+	// 		w.Write([]byte(r.URL.Path))
+	// 		return
+	// 	}
+	// default:
+	// 	w.Write([]byte("404 not found"))
+	// 	return
+	// }
 }
 
 func (s *api) CreateUsersHandler(w http.ResponseWriter, r *http.Request) {
@@ -45,9 +50,60 @@ func (s *api) DeleteUsersHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("User Deleted Succesfully"))
 }
 
+type User struct {
+	Name   string `json"name"`
+	Age    int `json"age"`
+	Gender string `json"gender"`
+}
+
+var users = []User{}
+
+func (s *api) GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(users)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusOK)
+
+}
+func (s *api) CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+	var payload User
+	err := json.NewDecoder(r.Body).Decode(&payload)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	u := User{
+		Name:   payload.Name,
+		Age:    payload.Age,
+		Gender: payload.Gender,
+	}
+	users = append(users, u)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("User Created Succesfully"))
+
+}
+
+// func insertUser(u User) error {
+// 	if u.Name == "" {
+// 		return errors.New("Name is Required")
+// 	}
+// 	if u.Age <= 0 {
+// 		return errors.New("Not a valid Age. ")
+// 	}
+
+// 	users = append(users, u)
+// 	return nil
+// }
+
 func main() {
 	api := &api{addr: ":8081"}
-	mux:= http.NewServeMux()
+	mux := http.NewServeMux()
 	srv := &http.Server{
 		Addr:    api.addr,
 		Handler: mux,
@@ -55,6 +111,7 @@ func main() {
 
 	mux.HandleFunc("POST /createUser", api.CreateUsersHandler)
 	mux.HandleFunc("POST /deleteUser", api.DeleteUsersHandler)
-	
+	mux.HandleFunc("GET /getUsers", api.GetUserHandler)
+	mux.HandleFunc("POST /createUser", api.CreateUserHandler)
 	srv.ListenAndServe()
 }
